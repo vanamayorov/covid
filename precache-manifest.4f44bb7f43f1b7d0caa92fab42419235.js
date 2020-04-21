e67bf14fee05c17c4a6c74a2aebc54d4087844a8
vanamayorov <denatarelcom2105@gmail.com>
2020-04-21 19:39:06 +0300

    Updates

---
+++ precache-manifest.4f44bb7f43f1b7d0caa92fab42419235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da240f1654f39348039d7f78b4e94f0b",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "e527c2a08a1ae9f2d2da",
+    "url": "/covid/static/css/main.5c51cbed.chunk.css"
+  },
+  {
+    "revision": "b792cd45a86c93637812",
+    "url": "/covid/static/js/2.33d68902.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.33d68902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e527c2a08a1ae9f2d2da",
+    "url": "/covid/static/js/main.867fd7f1.chunk.js"
+  },
+  {
+    "revision": "29d887c2aea3fb65dc6d",
+    "url": "/covid/static/js/runtime-main.b3dccd4d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/image.d7265326.png"
+  }
+]);
